555afce6d7f332531deac9f318a54a95a118a0cd
McGilly17 <4136843+mcgilly17@users.noreply.github.com>
2024-07-22 23:57:09 -0600

    re-add name

--- resources/vars.nix
+++ resources/vars.nix
@@ -5,6 +5,7 @@
       username = "michael";
       handle = "McGilly17";
       gitEmail = "4136843+mcgilly17@users.noreply.github.com";
+      userFullName = "Michael Paterson";
       # isMinimal = false; # Used to indicate nixos-installer build
     };
   };
