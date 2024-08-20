5e25321c5ad932afbceea79bd8ce4bbc0223e8e1
McGilly17 <4136843+mcgilly17@users.noreply.github.com>
2024-08-20 16:38:17 -0600

    update: configure icons for healines nvim

--- users/michael/common/tui/nvim/plugins/markdown/headlines.nix
+++ users/michael/common/tui/nvim/plugins/markdown/headlines.nix
@@ -2,6 +2,19 @@ _: {
   programs.nixvim = {
     plugins.headlines = {
       enable = true;
+      settings = {
+        markdown = {
+          headline_highlights = [
+            "Headline1"
+            "Headline2"
+            "Headline3"
+            "Headline4"
+            "Headline5"
+            "Headline6"
+          ];
+          bullets = ["󰎤" "󰎧" "󰎪" "󰎭" "󰎱" "󰎳"];
+        };
+      };
     };
   };
 }
