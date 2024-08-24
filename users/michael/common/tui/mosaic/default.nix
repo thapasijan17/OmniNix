ab124df390df55af41df50a1891c8d8db1e50a43
McGilly17 <4136843+mcgilly17@users.noreply.github.com>
2024-08-23 23:55:46 -0600

    feat: example of how to manage manually installed plugins in Mosiac

--- users/michael/common/tui/mosaic/default.nix
+++ users/michael/common/tui/mosaic/default.nix
@@ -11,6 +11,9 @@
         viAlias = true;
         vimAlias = true;
 
+        # Example of how to turn off manual plugins in Mosaic
+        # plugs.sidebar.enable = false;
+
         # Fugit2 needs to be installed here, this is due to its dependency on
         # libgit2 and how it needs to be linked (with the path)
         extraPlugins = with pkgs.vimUtils; [
