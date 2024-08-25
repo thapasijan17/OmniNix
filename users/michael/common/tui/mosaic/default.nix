7a43748f439dc7de87f2f0993fbc4feb21487829
McGilly17 <4136843+mcgilly17@users.noreply.github.com>
2024-08-24 22:43:53 -0600

    more examples of configuring mosaic

--- users/michael/common/tui/mosaic/default.nix
+++ users/michael/common/tui/mosaic/default.nix
@@ -14,6 +14,9 @@
         # Example of how to turn off manual plugins in Mosaic
         # plugs.sidebar.enable = false;
 
+        # Example of how to overwrite settings of plugins
+        # plugins.bufferline.settings.options.separator_style = "thick";
+
         # Fugit2 needs to be installed here, this is due to its dependency on
         # libgit2 and how it needs to be linked (with the path)
         extraPlugins = with pkgs.vimUtils; [
