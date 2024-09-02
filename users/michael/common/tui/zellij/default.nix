b833d21c3e59572cca0b4b42a14abeb2be2362f1
McGilly17 <4136843+mcgilly17@users.noreply.github.com>
2024-09-02 17:43:20 -0500

    feat: adding layout to zellij

--- users/michael/common/tui/zellij/default.nix
+++ users/michael/common/tui/zellij/default.nix
@@ -1,14 +1,14 @@
-let
+{pkgs, ...}: let
   shellAliases = {
     "zj" = "zellij";
   };
-in
-{
+in {
   programs.zellij = {
     enable = true;
   };
 
   xdg.configFile."zellij/config.kdl".source = ./config.kdl;
+  xdg.configFile."zellij/layouts/default.kdl".source = ./layouts/default.kdl;
 
   home.shellAliases = shellAliases;
 }
