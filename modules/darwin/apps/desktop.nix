3a2866058c1265d0b09009e3b197f1c77e02093e
McGilly17 <4136843+mcgilly17@users.noreply.github.com>
2024-09-11 17:45:14 -0600

    feat: adding affinity and altserver to desktop apps

--- modules/darwin/apps/desktop.nix
+++ modules/darwin/apps/desktop.nix
@@ -3,6 +3,11 @@
   homebrew = {
     casks = [
       "autodesk-fusion" # Autodesk CAD Software
+      "altserver" # Server for signging Apollo to my IOS
+      "adobe-creative-cloud" # For installing Lightroom
+      "affinity-designer"
+      "affinity-photo"
+      "affinity-publisher"
     ];
   };
 }
